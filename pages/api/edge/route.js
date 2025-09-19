6d595613d069cfa718f92fa55b7cadb1acbdb25d
nithya54 <nithyakamaraj19895@gmail.com>
2025-09-19 15:36:20 -0500

    main site commit

--- pages/api/edge/route.js
+++ pages/api/edge/route.js
@@ -8,11 +8,11 @@ export default function handler(request) {
   const pathname = url.pathname
   
   if (pathname.startsWith('/app1')) {
-    return fetch(`https://test-six-kohl-27.vercel.app${pathname}`)
+    return fetch(`https://myvercelwebsite-dusky.vercel.app${pathname}`)
   }
   
   if (pathname.startsWith('/app2')) {
-    return fetch(`https://your-second-app.vercel.app${pathname}`)
+    return fetch(`https://taskapp-sooty.vercel.app${pathname}`)
   }
   
   return new Response('Not found', { status: 404 })
